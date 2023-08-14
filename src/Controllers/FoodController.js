cdc56e7b092b65927953c58db037f1ffa849a0df
Henri <henriversiani@gmail.com>
2023-08-14 17:32:02 -0300

    add remove and search functions

--- src/Controllers/FoodController.js
+++ src/Controllers/FoodController.js
@@ -3,6 +3,21 @@ const sqliteConnection = require("../database/sqlite/");
 
 class foodController{
 
+    async searchPlates(request, response) {  
+
+
+        const { id_category } = request.params 
+        const { searchPlate } = request.query        
+    
+        const database = await sqliteConnection();
+
+        const param = '%'+searchPlate+'%'       
+
+        const lunchs = await database.all("SELECT * FROM plates WHERE id_category = (?) AND name like (?)", [id_category, param])   
+      
+        return response.status(201).json(lunchs)
+    }  
+
     async getLunchs(request, response) {  
 
         const { id_category } = request.params      
@@ -61,35 +76,66 @@ class foodController{
     }
 
 
-async update(request,response){
-    const { name, value} = request.body;
-    const user_id = request.user.id
-    const database = await sqliteConnection();
-    const food = await database.get("SELECT * FROM plates WHERE id = (?)", [id]);
+    async update(request,response){
 
+        const {plateName, price, description, idCategory, tags} = request.body    
+        const user_id = request.user.id   
+        const {id_plate} = request.params
+
+        const database = await sqliteConnection();  
+
+        console.log(user_id, id_plate)
+
+        try {        
+            await database.run(`
+            UPDATE plates SET 
+            name= ?,
+            value = ?,
+            description = ?,
+            id_category = ?,
+            id_user = ?,
+            updated_at = ?
+            WHERE id = ?`,
+            [plateName, price, description, idCategory, user_id, new Date(), id_plate]);
+
+
+            tags.forEach(async (tag) => {      
+                
+                await database.run("delete from plates_tags where id_plate = ?" , [id_plate])
+
+                await database.run("INSERT INTO plates_tags (name, id_plate) VALUES (?, ?)" , [tag, id_plate])
+        
+            }) 
+
+
+        } catch (error) {
+            throw new AppError ("SQL ERROR ",error);
+        }
+
+        return response.status(200).json()
+    }
+
+
+    async delete(request,response){
+      
+        const {id_plate} = request.params
     
+        const database = await sqliteConnection(); 
+    
+        try {     
 
-    if(!food){
-        throw new AppError ("Prato nao encontrado");
-    }
-
-food.name = name;
-food.value = value;
-
-    try {
-        await database.run(`
-    UPDATE plates SET 
-    name= ?,
-    value = ?,
-    updated_at = ?
-    WHERE id = ?`,
-    [food.name, food.value, new Date(), id]);
-    } catch (error) {
-        throw new AppError ("SQL ERROR ",error);
+            await database.run(`DELETE from plates WHERE id = ?`, [ id_plate]);     
+    
+        } catch (error) {
+            throw new AppError ("SQL ERROR ",error);
+        }
+    
+        return response.status(200).json()
     }
 
 
-return response.status(200).json()
-}}
+}
+
+
 
 module.exports = foodController;  
--- src/routes/food.routes.js
+++ src/routes/food.routes.js
@@ -13,7 +13,8 @@ const foodAvatarController = new FoodAvatarController()
 const foodController = new FoodController()
 
 foodRoutes.post("/" , ensureAuthenticated, foodController.create)
-foodRoutes.put("/", ensureAuthenticated, foodController.update)
+
+foodRoutes.put("/update/:id_plate", ensureAuthenticated, foodController.update)
 
 foodRoutes.get("/foods/:id_category" , ensureAuthenticated, foodController.getLunchs)
 
@@ -21,5 +22,8 @@ foodRoutes.get("/detail/:id_plate" , ensureAuthenticated, foodController.getLunc
 
 foodRoutes.patch("/avatar/:plate_id", upload.single("avatar"), foodAvatarController.update)
 
+foodRoutes.delete("/delete/:id_plate", foodController.delete)
+
+foodRoutes.get("/search/category/:id_category", foodController.searchPlates)
 
 module.exports = foodRoutes;
